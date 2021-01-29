a621b41ad3aae0b80ed28272ee4582462135451a
Eduardo F <eduardofiorezi@gmail.com>
2021-01-28 23:30:48 -0300

    Rename checker method

--- lib/splittable/division.rb
+++ lib/splittable/division.rb
@@ -5,12 +5,11 @@ class Splittable::Division
     @value = BigDecimal(value.truncate(2), 15)
     @quantity = BigDecimal(quantity.to_i, 15)
 
-    quantity_positive_validation!
+    check_quantity_as_positive_value!
   end
 
   def call
     partial_value = (value / quantity).truncate(2)
-
     installments = [partial_value] * quantity
     installments[0] += value - installments.sum.to_d
     installments
@@ -20,7 +19,7 @@ class Splittable::Division
 
   attr_reader :value, :quantity
 
-  def quantity_positive_validation!
+  def check_quantity_as_positive_value!
     return if quantity.positive?
 
     raise ArgumentError, 'quantity should be positive'
